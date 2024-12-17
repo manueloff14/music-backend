f11fb46195f0d13d665147559dab0923f6d54db1
manueloff14 <manueloffcabrera@gmail.com>
2024-12-16 21:32:31 -0500

    cambiamos las .env

--- app.js
+++ app.js
@@ -6,8 +6,22 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const admin = require('firebase-admin');
 
+console.log('SERVICE_ACCOUNT_PRIVATE_KEY:', process.env.SERVICE_ACCOUNT_PRIVATE_KEY);
+
 // Inicializar Firebase Admin usando la variable de entorno
-const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT_JSON);
+const serviceAccount = {
+    "type": process.env.SERVICE_ACCOUNT_TYPE,
+    "project_id": process.env.SERVICE_ACCOUNT_PROJECT_ID,
+    "private_key_id": process.env.SERVICE_ACCOUNT_PRIVATE_KEY_ID,
+    "private_key": process.env.SERVICE_ACCOUNT_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    "client_email": process.env.SERVICE_ACCOUNT_CLIENT_EMAIL,
+    "client_id": process.env.SERVICE_ACCOUNT_CLIENT_ID,
+    "auth_uri": process.env.SERVICE_ACCOUNT_AUTH_URI,
+    "token_uri": process.env.SERVICE_ACCOUNT_TOKEN_URI,
+    "auth_provider_x509_cert_url": process.env.SERVICE_ACCOUNT_AUTH_PROVIDER_X509_CERT_URL,
+    "client_x509_cert_url": process.env.SERVICE_ACCOUNT_CLIENT_X509_CERT_URL,
+    "universe_domain": process.env.SERVICE_ACCOUNT_UNIVERSE_DOMAIN
+};
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
